8f79e8d1e258fa822faef183029b996222e4e5a9
Tabea <JungfrauIsadoro@mobile-199-174.wlan.uni-mannheim.de>
2019-10-02 15:02:17 +0200

    add comments

--- data_scraping/scrape_and_union_transfers.py
+++ data_scraping/scrape_and_union_transfers.py
@@ -18,24 +18,30 @@ transfers = pd.DataFrame()
 
 for url in urls:
     try:
+        # try to open website if it exists
         page = urllib.request.urlopen(url)
     except:
         pass
     soup = BeautifulSoup(page)
+    # get rid of html language to have the plain data
     data_string = str(soup).replace('<html><body><p>', '').replace('</p></body></html>', '')
+    # clean data of npn-field-separating commata 
     data_string = re.sub(r'(End of loan\w{3}\s\d\d*)(,)(\s\d{4})', r'\1.\3', data_string)
+    # create dataframe
     rows = data_string.splitlines()
     df = pd.DataFrame([row.split(",") for row in rows])
     df.columns = df.iloc[0]
     df = df[1:]
+    # concatenate dataframes
     if transfers.empty:
         transfers = df.copy()
     else:
         transfers = pd.concat([transfers, df])
 
+# delete unclean data and ensure order of columns
 transfers = transfers[transfers['year'] != 'Liga Nos']
 transfers = transfers[['club_name', 'player_name', 'age', 'position', 'club_involved_name',
        'fee', 'transfer_movement', 'fee_cleaned', 'league_name', 'year',
        'season']]
 
-transfers.to_csv('data/transfers.csv', index = False)
+transfers.to_csv('../data/transfers.csv', index = False)
