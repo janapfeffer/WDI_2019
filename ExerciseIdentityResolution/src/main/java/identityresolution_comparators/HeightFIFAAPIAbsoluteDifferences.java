5cdada3bbf32657844c2945dd68e8865843c37b7
maida <maida.nazifi@gmail.com>
2019-11-12 14:14:26 +0100

    added HeightFIFAAPIAbsoluteDifference

--- ExerciseIdentityResolution/src/main/java/identityresolution_comparators/HeightFIFAAPIAbsoluteDifferences.java
+++ ExerciseIdentityResolution/src/main/java/identityresolution_comparators/HeightFIFAAPIAbsoluteDifferences.java
@@ -6,17 +6,15 @@ import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.similarity.numeric.AbsoluteDifferenceSimilarity;
-import de.uni_mannheim.informatik.dws.winter.similarity.numeric.DeviationSimilarity;
-import de.uni_mannheim.informatik.dws.winter.similarity.numeric.PercentageSimilarity;
 import identityresolution_models.Player;
 
-public class HeightFIFAESDDeviationSimilarity implements Comparator<Player, Attribute>{
+public class HeightFIFAAPIAbsoluteDifferences implements Comparator<Player, Attribute>{
 	
 	//similarity measure test
-	public static void main( String[] args ) throws Exception{
-	System.out.println(sim.calculate(170.76, 165.98));
+	 public static void main( String[] args ) throws Exception{
+		System.out.println(sim.calculate(170.76, 165.98));
 	}
-	private static DeviationSimilarity sim = new DeviationSimilarity();
+	private static AbsoluteDifferenceSimilarity sim = new AbsoluteDifferenceSimilarity(8);
 	
 	private ComparatorLogger comparisonLog;
 
@@ -28,38 +26,39 @@ public class HeightFIFAESDDeviationSimilarity implements Comparator<Player, Attr
 		
 		//get height values
 		double fifa_height;
-		double esd_height;
+		double api_height;
 		try {
 			fifa_height = fifaplayer.getHeight();
 		} catch (Exception e) {
 			fifa_height = 0;
 		}
 		try {
-			esd_height = esdplayer.getHeight();
+			api_height = esdplayer.getHeight();
 		} catch (Exception e) {
-			esd_height = 0;
+			api_height = 0;
 		}
+		
 		 
 
 		// save names and class name to logger
 		if(this.comparisonLog != null){
 			this.comparisonLog.setComparatorName(getClass().getName());
 			this.comparisonLog.setRecord1Value(String.valueOf(fifa_height));
-			this.comparisonLog.setRecord2Value(String.valueOf(esd_height));
+			this.comparisonLog.setRecord2Value(String.valueOf(api_height));
 		}
 
 		
 	    // calculate similarity
 		double similarity;
 		try {
-			similarity = sim.calculate(fifa_height, esd_height);
+			similarity = sim.calculate(fifa_height, api_height);
 		} catch (Exception e) { // set similarity to zero in case it is missing in one of the datasets
 			similarity = 0;
 		}
 
 		if(this.comparisonLog != null){
 			this.comparisonLog.setRecord1PreprocessedValue(Double.toString(fifa_height));
-			this.comparisonLog.setRecord2PreprocessedValue(Double.toString(esd_height));
+			this.comparisonLog.setRecord2PreprocessedValue(Double.toString(api_height));
 
 			this.comparisonLog.setSimilarity(Double.toString(similarity));
 			//this.comparisonLog.setPostprocessedSimilarity(Double.toString(postSimilarity));
