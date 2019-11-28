14ddf79d98f46329d805442391d7dfcc09845734
Jana Lotta Pfeffer <jana.lotta.pfeffer01@sap.com>
2019-11-28 18:18:15 +0100

    wage evaluation

--- FootballWebDataIntegration/src/main/java/fusion_evaluation/WageEvaluationRule.java
+++ FootballWebDataIntegration/src/main/java/fusion_evaluation/WageEvaluationRule.java
@@ -4,6 +4,7 @@ import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.numeric.PercentageSimilarity;
 import models.Player;
 /**
  * @author group3
@@ -11,17 +12,18 @@ import models.Player;
  * Evaluation of the fusion of the wage.
  */
 public class WageEvaluationRule extends EvaluationRule<Player, Attribute>  {
+	private static PercentageSimilarity sim = new PercentageSimilarity(10);
 
 	@Override
 	public boolean isEqual(Player record1, Player record2, Attribute schemaElement) {
-		// only FIFA19 has the wage
-		return record1.getWage() == record2.getWage();
+		// since wages fluctuate quite a lot we accept a difference of 10%
+		return sim.calculate((double) record1.getWage(), (double) record2.getWage()) == 1.0;
 	}
 
 	@Override
 	public boolean isEqual(Player record1, Player record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
-		// the wage information in FIFA19 is not the same as the most recent wages
-		return record1.getWage() == record2.getWage();
+		// only FIFA19 has the wage
+		return isEqual(record1, record2, (Attribute)null);
 	}
 
 }
